3929bb7bd73b00cbcd3f98abed4d9b1c1ba04a96
Pablo <pablojacobo8899@gmail.com>
2021-07-30 23:56:52 -0700

    Figured out how to print to console my results

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,30 @@ function computerPlay() {
     } else if (number >= 4) {
         return "Paper";
     }
-    return "Scissors" 
+    return "Scissors";
 }
-console.log(computerPlay())
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = prompt("Rock, paper, or scissor?").toLowerCase();
+    if (computerSelection === "Rock" && playerSelection === "scissor") {
+        return "You lose, rock beats scissor!";
+    } else if (computerSelection === "Paper" && playerSelection === "rock") {
+        return "You lose, paper beats rock!";
+    } else if (computerSelection === "Scissors" && playerSelection === "paper") {
+        return "You lose, scissors beat paper!"
+    } else if (computerSelection === "Scissors" && playerSelection === "rock") {
+        return "You win, rock beats scissors!";
+    } else if (computerSelection === "Scissors" && playerSelection === "scissors") {
+        return "It's a tie!";
+    } else if (computerSelection === "Rock" && playerSelection === "rock") {
+        return "It's a tie, theyre the same!";
+    } else if (computerSelection === "Paper" && playerSelection === "paper") {
+        return "It's a tie, theyre both paper!";
+    } else if (computerSelection === "Paper" && playerSelection === "scissor") {
+        return "You win, scissor beats paper!";
+    } else if (computerSelection === "Rock" && playerSelection === "paper") {
+        return "You win, paper beats rock!";
+    }
+}    
+console.log(playRound());
