60926a9c2e731a363bae29d75765cd74e1b646dd
Pablo <pablojacobo8899@gmail.com>
2021-07-31 23:52:38 -0700

    After some help from the people in the odin project discord, i was able to finish this project! - Finalized the function game such that it would run correctly and alert the player if theyd won or lost

--- scripts/script.js
+++ scripts/script.js
@@ -5,41 +5,56 @@ function computerPlay() {
     } else if (number >= 4) {
         return "Paper";
     }
-    return "Scissors";
+    return "Scissor";
 }
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    playerSelection = prompt("Rock, paper, or scissor?").toLowerCase();
-    if (playerSelection === null) {
-        return "Please make a selection";
-    } else if (computerSelection === "Rock" && playerSelection === "scissor") {
-        return "You lose, rock beats scissor!";
+let playerScore = 0
+let computerScore = 0
+
+
+function playRound(computerSelection, playerSelection) {
+      if (computerSelection === "Rock" && playerSelection === "scissor") {
+      	++computerScore;
+        return `You lose, rock beats scissor! The score is now ${computerScore} for the computer and ${playerScore} for you.`
     } else if (computerSelection === "Paper" && playerSelection === "rock") {
-        return "You lose, paper beats rock!";
-    } else if (computerSelection === "Scissors" && playerSelection === "paper") {
-        return "You lose, scissors beat paper!"
-    } else if (computerSelection === "Scissors" && playerSelection === "rock") {
-        return "You win, rock beats scissors!";
-    } else if (computerSelection === "Scissors" && playerSelection === "scissors") {
-        return "It's a tie!";
+    	++computerScore;
+        return `You lose, paper beats rock! The score is now ${computerScore} for the computer and ${playerScore} for you.`;
+    } else if (computerSelection === "Scissor" && playerSelection === "paper") {
+    	++computerScore;
+        return `You lose, scissors beat paper! The score is now ${computerScore} for the computer and ${playerScore} for you.`;
+    } else if (computerSelection === "Scissor" && playerSelection === "rock") {
+    	++playerScore;
+        return `You win, rock beats scissors! The score is now ${computerScore} for the computer and ${playerScore} for you.`;
+    } else if (computerSelection === "Scissor" && playerSelection === "scissor") {
+        return `It's a tie!`;
     } else if (computerSelection === "Rock" && playerSelection === "rock") {
-        return "It's a tie, theyre the same!";
+        return `It's a tie, theyre the same!`;
     } else if (computerSelection === "Paper" && playerSelection === "paper") {
-        return "It's a tie, theyre both paper!";
+        return `It's a tie, theyre both paper!`;
     } else if (computerSelection === "Paper" && playerSelection === "scissor") {
-        return "You win, scissor beats paper!";
+    	++playerScore;
+        return `You win, scissor beats paper! The score is now ${computerScore} for the computer and ${playerScore} for you.`;
     } else if (computerSelection === "Rock" && playerSelection === "paper") {
-        return "You win, paper beats rock!";
+    	++playerScore;
+        return `You win, paper beats rock! The score is now ${computerScore} for the computer and ${playerScore} for you.`
     }
 }   
 
-
 function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log(playRound()); 
-    }
+	for (let i = 0; i < 5; i++) {
+	computerSelection = computerPlay();
+	playerSelection = prompt("Rock, paper, or scissor?").toLowerCase();
+	console.log(playRound(computerSelection, playerSelection));
+   } if (playerScore > computerScore) {
+       console.log("The human wins!");
+       alert("The human wins!");
+   } else if (playerScore < computerScore) {
+       console.log("The computer wins!");
+       alert("The computer wins!");
+   } else if (playerScore == computerScore) {
+       console.log("It's a tie! Try again!"); 
+       alert("It's a tie! Try again!");
+   }
 }
 
 game();
